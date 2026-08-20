b62cf8e2d3e5e83f2dd0d292a861308b8c5925d1
ridmonsan <ridwan.montclar@gmail.com>
2026-08-20 20:03:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// SALIN file ini menjadi config.js dan isi 2 nilai dari Supabase.
+// Jangan pernah memasukkan service_role key ke website/browser.
+// Yang boleh dipakai di frontend adalah anon/public key.
+
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://zfgesaywlyvljjksvbji.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_M824qcT2YAUaq1v7yEsc6g_NK-cZ1tJ"
+};
